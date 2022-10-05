0d6cb0fa7bb6feeb2479f17caf40fb53efba86ee
Myeonghyeon Son <franksmh0409@gmail.com>
2022-10-05 22:41:23 +0900

    Trader updated

--- Algorithm/KingGangSan/BruteForce/BJ_1182.py
+++ Algorithm/KingGangSan/BruteForce/BJ_1182.py
@@ -12,6 +12,7 @@ combinations를 활용하는 것이 맞나?
 '''
 
 ## 부분합을 구한 뒤 for문 두 개 사용하면 풀 수 있을 듯?
+## 이거 안되는 이유 : 연속된 부분 수열이 아니어도 되기 때문..! 후우.
 # n, s = list(map(int, input().split()))
 # if n == 1:
 #     seq = int(input())
@@ -40,15 +41,18 @@ combinations를 활용하는 것이 맞나?
 #     else:
 #         print(0)
 
+
+# itertools 의 combinations를 활용한 방법
+'''
 from itertools import combinations
 
 
-n, s = list(map(int, input().split()))
+n, s = list(map(int, input().split())) # list로 안 묶어도 됨.
 seq = list(map(int, input().split()))
 
 res = 0
 for i in range(1, n+1):
-    combs = list(combinations(seq, i))
+    combs = list(combinations(seq, i)) # list로 안 묶어도 된다. combinations 객체는 그 자체로 이미 iterator니까.
 
     for comb in combs:
         summ = sum(comb)
@@ -56,3 +60,34 @@ for i in range(1, n+1):
             res += 1
 
 print(res)
+'''
+
+
+# 재귀함수를 활용하는 방법
+n, s = map(int, input().split())
+seq = list(map(int, input().split()))
+
+cnt = 0
+def sub_sum(idx, sum_former):
+    # global cnt # global 변수를 사용하지 않으면 조금 어려운 듯. 왜냐? 재귀함수 갈래가 계속해서 2개로 나뉘어지는데, 그 모든 재귀함수들의 결과값을 하나의 값에 담아야 함.
+    # 아니면, 전부를 더하는 게 아니라. return되는 값이 이때까지의. 모든 재귀함수들의 cnt를 더한 값. 이라고 할 수도 있을 듯.
+
+    tmp_cnt = 0
+    if idx==n-1:
+        if sum_former+seq[idx] == s:
+            tmp_cnt += 1
+
+
+        return tmp_cnt
+
+    else:
+        idx2 = idx + 1
+        subSum = sum_former + seq[idx]
+        if subSum == s:
+            return sub_sum(idx2, subSum) + sub_sum(idx2, sum_former) + 1
+        else:
+            return sub_sum(idx2, subSum) + sub_sum(idx2, sum_former)
+
+print(sub_sum(0, 0))
+
+# 훨씬 빠르고 메모리도 덜 사용한다. 이유가 뭘까?? 적은 개수의 수열을 사용해서?
---
+++ AutoTrader/BinanceTrader/binance.md
@@ -0,0 +1,28 @@
+1. Backtrading data
+interval:
+m -> minutes; h -> hours; d -> days; w -> weeks; M -> months
+- 1m
+- 3m
+- 5m
+- 15m
+- 30m
+- 1h
+- 2h
+- 4h
+- 6h
+- 8h
+- 12h
+- 1d
+- 3d
+- 1w
+- 1M
+Update Speed: 250ms
+
+최대 1h정도의 interval로 데이터를 가져와서 backtrading한다.
+
+2. Strategy
+  1. MA돌파시 매매
+  2. 변동성 돌파
+  3. 그냥 간단하게 노가다로, 몇% 이상 먹으면 시장가 매도
+  4. 수수료를 꼭 생각해야 함. 수수료는 어떻게 날아가는지
+
