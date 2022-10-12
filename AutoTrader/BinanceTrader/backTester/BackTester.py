4b5fe22c2365b26604ede41fff45b78d5596cd14
Myeonghyeon Son <franksmh0409@gmail.com>
2022-10-12 16:01:43 +0900

    updated backTester

---
+++ AutoTrader/BinanceTrader/backTester/BackTester.py
@@ -0,0 +1,91 @@
+# 전략용 library
+import time
+import requests
+import numpy as np
+import pandas as pd
+import logging
+import datetime
+from binance.um_futures import UMFutures
+from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
+from binance.lib.utils import config_logging
+
+# API KEY 저장된 곳
+from cert import binanceKey
+
+# 시각화 library
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class BackTester():
+    def __init__(self, asset=1000, maxLimit=1000): # default asset : 1000 dollars
+        # vars for long position
+        self.long_amount = 0
+        self.long_meanPrice = 0
+        self.long_flag = False
+        # vars for short position
+        self.short_amount = 0
+        self.short_meanPrice = 0
+        self.short_flag = False
+        # vars for base asset
+        self.asset = asset
+        self.PRINCIPAL = asset # constant
+        self.maxLimit = maxLimit
+        # var for busted (when did you bust?)
+        self.bustedIndex = 0
+
+    def _get_tradeAmount(self, tradePrice):
+        tradeAmount = round(self.maxLimit/tradePrice/2, 4)
+        return tradeAmount
+
+    def set_long(self, tradePrice):
+        tradeAmount = self._get_tradeAmount(tradePrice)
+        self.maxLimit -= tradeAmount * tradePrice
+        self.long_meanPrice = (self.long_meanPrice*self.long_amount + tradePrice*tradeAmount)/(self.long_amount + tradeAmount) # 이거 맞아?
+        self.long_amount += tradeAmount
+        self.long_flag = True
+        return
+
+    def set_short(self, tradePrice):
+        tradeAmount = self._get_tradeAmount(tradePrice)
+        self.maxLimit -= tradeAmount * tradePrice
+        self.short_meanPrice = (self.short_meanPrice*self.short_amount + tradePrice*tradeAmount)/(self.short_amount + tradeAmount)
+        self.short_amount += tradeAmount
+        self.short_flag = True
+        return
+
+    def set_clear(self, tradePrice):
+        self.asset -= self.short_amount * (tradePrice - self.short_meanPrice)
+        self.asset += self.long_amount * (tradePrice - self.long_meanPrice)
+        self.short_amount = self.long_amount = self.short_meanPrice = self.long_meanPrice = 0
+        self.long_flag = self.short_flag = False
+        self.maxLimit = self.asset
+        return
+
+    def check_asset_positive(self, tradePrice):
+        tmpAsset = self.asset - (self.short_amount * (tradePrice - self.short_meanPrice))
+        tmpAsset = self.asset + (self.long_amount * (tradePrice - self.long_meanPrice))
+        if tmpAsset < 0:
+            return False
+        else:
+            return True
+
+    def get_log(self, index, stateTxt):
+        print(f'#########################')
+        print(f'## index {index}, {stateTxt}')
+        print(f'#########################')
+
+        if stateTxt != 'Busted':
+            print(f'long amount:{self.long_amount}')
+            print(f'long meanPrice:{self.long_meanPrice}')
+            print(f'short amount:{self.short_amount}')
+            print(f'short meanPrice:{self.short_meanPrice}')
+            print(f'asset:{self.asset}')
+            print(f'maxLimit:{self.maxLimit}')
+            self.bustedIndex = index
+
+    def get_current_return(self, index):
+        print(f'**current return : {self.asset/self.PRINCIPAL*100}%')
+        return self.asset / self.PRINCIPAL
+
+    def get_commission(self):
+        pass
---
+++ AutoTrader/BinanceTrader/backTester/DataGen.py
@@ -0,0 +1,47 @@
+# 전략용 library
+import time
+import requests
+import numpy as np
+import pandas as pd
+import logging
+import datetime
+from binance.um_futures import UMFutures
+from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
+from binance.lib.utils import config_logging
+
+# API KEY 저장된 곳
+from cert import binanceKey
+
+# 시각화 library
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+def get_backdata(client, interval, **kwargs): # symbol은 필요 없음, interval, startTime, endTime, limit만 있으면 된다.
+    rawbackdata = client.klines('BTCUSDT', interval, **kwargs)
+    columns = ['OpenTime', 'Open', 'High', 'Low', 'Close', 'Volume', 'CloseTime', 'QuoteAssetVolume', 'NumOfTrades',
+               'TakerBuyBaseAssetVolume', 'TakerBuyQuoteAssetVolume', 'Ignore']
+    backdatadf = pd.DataFrame(rawbackdata, columns=columns)
+    backdatadf['OpenTime'] = backdatadf['OpenTime'].astype("datetime64[ms]")
+    backdatadf['CloseTime'] = backdatadf['CloseTime'].astype("datetime64[ms]")
+    backdatadf[['Open', 'High', 'Low', 'Close', 'Volume', 'QuoteAssetVolume', 'TakerBuyBaseAssetVolume', 'TakerBuyQuoteAssetVolume']] = backdatadf[['Open', 'High', 'Low', 'Close', 'Volume', 'QuoteAssetVolume', 'TakerBuyBaseAssetVolume', 'TakerBuyQuoteAssetVolume']].astype('float64')
+    backdatadf.drop(['Ignore'], axis=1, inplace=True)
+    return backdatadf
+
+# backdata와 window, Close(종가)의 이름을 주면 MA{window}의 열을 반환하는 것
+# 반환된 열을 Df에 추가할 수도 있고, 새로 만들 수도 있고...
+# 열을 변환하는 경우에는 항상 반환은 열로 되도록 하기.
+def get_MA(dataDf, window, CloseName='Close'):
+    return dataDf[CloseName].astype(np.float64).rolling(window).mean()
+
+def get_std(dataDf, window, CloseName='Close'):
+    return dataDf[CloseName].astype(np.float64).rolling(window).std()
+
+# Series 여러 개를 합쳐서 하나의 Df로 병합하는 함수
+def concat_Series(*series):
+    return pd.concat(series, axis=1)
+
+
+# client = UMFutures()
+# client.depth("BTCUSDT", **{"limit": 5})
